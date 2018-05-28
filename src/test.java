2186f2e093fd69ce926a8235839cebcc9acc0758
Mateusz Barzantny <mateusz.barzantny@op.pl>
2018-05-28 20:06:23 +0200

    2nd commit

--- src/test.java
+++ src/test.java
@@ -3,16 +3,30 @@ public class test {
     public static void main(String[] args) {
         Triangle prostokatny = new Triangle("Trojkat","prostokątny");
         Triangle rownoramienny = new Triangle("Trójkąt", "rownoramienny");
+        Triangle jakis = new Triangle("trójkąt","jakiś");
+        System.out.println("to jest" + jakis.getTyp() + " " + jakis.getNazwa());
 
-        System.out.println(prostokatny.getNazwa()+ " "+prostokatny.getTyp());
-        System.out.println(rownoramienny.getNazwa()+" "+ rownoramienny.getTyp());
+        Model model3 = new Model();
+        boolean licz = model3.isRightTriangle(jakis);
+        System.out.println(licz);
+        jakis.setTyp("prostokątny");
+        boolean licz2 = model3.isRightTriangle(jakis);
+        System.out.println("a po zmianie kątów" + " " + licz2);
+
+        System.out.print(prostokatny.getNazwa()+ " "+prostokatny.getTyp());
+
         Model model = new Model();
         boolean liczydlo = model.isRightTriangle(rownoramienny);
-        System.out.println(liczydlo);
+        System.out.println(" " + liczydlo);
+
+        System.out.print(rownoramienny.getNazwa()+" "+ rownoramienny.getTyp());
+
 
         Model model2 = new Model();
         boolean liczydlo2 = model2.isRightTriangle(prostokatny);
-        System.out.println(liczydlo2);
+        System.out.println(" " + liczydlo2);
+
+
     }
 
 
